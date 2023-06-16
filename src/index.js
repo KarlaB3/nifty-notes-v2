07b7cde8f8494d5f960dac165c8485d25e80e407
KarlaB3 <k.bucoy@gmail.com>
2023-06-16 16:47:06 +1000

    build: set up initial environment

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const {app} = require('./server');
+
+app.listen(3007, () => {
+	console.log("Server is running");
+});
---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+app.use(express.json());
+
+async function dbConnect(){
+	try{
+		await mongoose.connect('mongodb://localhost:27017/nifty_notes_db')
+		console.log("Database connected!")
+	} catch (error) {
+		console.log(`dbConnect failed, error: ${JSON.stringify(error)}`)
+	}
+}
+
+dbConnect()
+
+app.get("/", (request, response) => {
+	response.json({
+		message:"Welcome to the note taking backend"
+	});
+});
+
+const notesRouter = require('./routes/notes_routes')
+app.use("/notes", notesRouter)
+
+module.exports = {
+	app
+}
